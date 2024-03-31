72f4f67a3e365cc4c1c15540ceadd6da666b5e23
ahmedsamy2003 <70441494+ahmedsamy2003@users.noreply.github.com>
2024-03-31 05:51:37 +0200

    feat: add theme provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,18 @@
 import Login from "@/views/Login";
 import { useState } from "react";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "@/components/mode-toggle";
 
 function App() {
   const [id, setId] = useState("");
 
   return (
-    <>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="absolute right-5 top-5">
+        <ModeToggle />
+      </div>
       <Login onIdSubmit={setId} />
-    </>
+    </ThemeProvider>
   );
 }
 
